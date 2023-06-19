94627d9d9013ee5801ed5e20b109535727779c03
Code Sandbox <hello@codesandbox.io>
2023-06-19 04:36:33 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.52fe2a038bbfeaed5aeaaf45d0515ec6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "418c32cc0fd7ecca366fccbd7c5d31c2",
+    "url": "https://varis2802.github.io/csb-fz8wjt/index.html"
+  },
+  {
+    "revision": "1433b684ea123ee0f478",
+    "url": "https://varis2802.github.io/csb-fz8wjt/static/css/main.b122625a.chunk.css"
+  },
+  {
+    "revision": "59d09b483e9a75e76997",
+    "url": "https://varis2802.github.io/csb-fz8wjt/static/js/2.2b6ef4fe.chunk.js"
+  },
+  {
+    "revision": "1433b684ea123ee0f478",
+    "url": "https://varis2802.github.io/csb-fz8wjt/static/js/main.65fe6533.chunk.js"
+  },
+  {
+    "revision": "85e0a86041fad8f35d5f",
+    "url": "https://varis2802.github.io/csb-fz8wjt/static/js/runtime~main.ec6476f7.js"
+  }
+]);
